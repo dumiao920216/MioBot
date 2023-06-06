1770272812f92d15c0e9f1830400023970f5be1d
Daitomio <dumiao920216@qq.com>
2023-06-06 19:05:26 +0800

    更换每日新闻接口

--- Func/DailyNews.cs
+++ Func/DailyNews.cs
@@ -12,7 +12,7 @@ namespace MioBot.Func
         public static JsonObject Get()
         {
             var httpClient = new HttpClient();
-            var response = httpClient.GetAsync("https://api.emoao.com/api/60s?type=json").Result.Content;
+            var response = httpClient.GetAsync("http://bjb.yunwj.top/php/tp/lj.php").Result.Content;
             var str = response.ReadAsStringAsync().Result.ToString();
             var jsobj = JsonNode.Parse(str)!.AsObject();
             return jsobj;
---
+++ Func_Ack/Picture.cs
@@ -0,0 +1,27 @@
+﻿using MioBot.Bot;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace MioBot.Func_Ack
+{
+    internal class Picture
+    {
+        public static void Push(string group, string qq, string keyword)
+        {
+            //应答
+            //Qmsg.Group(group, "@at=" + qq + "@ 正在为你寻找"+keyword+"的美图，请稍后~");
+            //var httpClient = new HttpClient();
+            //var requesturl = "http://bjb.yunwj.top/php/tk/sj.php?mc=" + keyword;
+            //var response = httpClient.GetAsync(requesturl).Result.Content;
+            //var stream = response.ReadAsStreamAsync().Result;
+            //var jpg = Image.FromStream(stream);
+            //var str = "@at=" + qq + "@ 喵，喵喵喵！喵喵~";
+            //Qmsg.Group(group, msgList[r]);
+        }
+    }
+}
